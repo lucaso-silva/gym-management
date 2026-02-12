f905d592b890ed1d0202b302395677c3bb3cae4a
Lucas Oliveira <luc.osilva@gmail.com>
2026-02-12 09:50:17 -0800

    refactor(exception): add NotFoundException

---
+++ src/main/java/com/lucas/gym_management/application/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.lucas.gym_management.application.exceptions;
+
+public class NotFoundException extends ApplicationException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
