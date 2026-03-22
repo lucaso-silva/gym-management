35be0d025b375c8e5240d68f17ac2b43b3a39363
Lucas Oliveira <luc.osilva@gmail.com>
2026-03-22 13:51:37 -0700

    add GlobalExceptionHandler to controller

---
+++ src/main/java/com/lucas/gym_management/application/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.lucas.gym_management.application.exceptions;
+
+public class ConflictException extends ApplicationException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
