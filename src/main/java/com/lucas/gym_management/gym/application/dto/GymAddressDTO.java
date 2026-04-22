8c23d30ea9ab7d467fa60912bde29e55156e3522
Lucas Oliveira <luc.osilva@gmail.com>
2026-04-22 20:18:27 -0300

    enforce input validation on Gym creation DTOs

--- src/main/java/com/lucas/gym_management/gym/application/dto/GymAddressDTO.java
+++ src/main/java/com/lucas/gym_management/gym/application/dto/GymAddressDTO.java
@@ -1,11 +1,17 @@
 package com.lucas.gym_management.gym.application.dto;
 
 import com.lucas.gym_management.gym.application.domain.model.valueObjects.GymAddress;
+import jakarta.validation.constraints.NotBlank;
 
-public record GymAddressDTO(String street,
+public record GymAddressDTO(@NotBlank(message = "Street cannot be blank")
+                            String street,
+                            @NotBlank(message = "Number cannot be blank")
                             String number,
+                            @NotBlank(message = "Neighborhood cannot be balnk")
                             String neighborhood,
+                            @NotBlank(message = "City cannot be blank")
                             String city,
+                            @NotBlank(message = "State cannot be blank")
                             String state) {
     public static GymAddressDTO from(GymAddress address) {
         return new GymAddressDTO(address.getStreet(),
--- src/main/java/com/lucas/gym_management/gym/application/ports/inbound/create/CreateGymInput.java
+++ src/main/java/com/lucas/gym_management/gym/application/ports/inbound/create/CreateGymInput.java
@@ -2,9 +2,13 @@ package com.lucas.gym_management.gym.application.ports.inbound.create;
 
 import com.lucas.gym_management.gym.application.dto.GymAddressDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record CreateGymInput(String name,
+public record CreateGymInput(@NotBlank(message = "Name cannot be blank")
+                             String name,
+                             @NotBlank(message = "Phone cannot be blank")
                              String phone,
-                             @Valid @NotNull GymAddressDTO address) {
+                             @Valid @NotNull(message = "Address cannot be null")
+                             GymAddressDTO address) {
 }
