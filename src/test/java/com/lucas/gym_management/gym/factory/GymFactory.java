63a2616c2ef42fa9060e739fb77f3ac58081dab0
Lucas Oliveira <luc.osilva@gmail.com>
2026-04-24 22:18:48 -0300

    add unit tests for ListGymsUseCase

--- src/test/java/com/lucas/gym_management/gym/factory/GymFactory.java
+++ src/test/java/com/lucas/gym_management/gym/factory/GymFactory.java
@@ -5,6 +5,8 @@ import com.lucas.gym_management.gym.application.domain.model.valueObjects.GymAdd
 import com.lucas.gym_management.gym.application.dto.GymAddressDTO;
 import com.lucas.gym_management.gym.application.ports.inbound.create.CreateGymInput;
 
+import java.util.List;
+
 public class GymFactory {
     public static CreateGymInput buildCreateGymInput() {
         return new CreateGymInput(
@@ -26,5 +28,25 @@ public class GymFactory {
                         "any-neighborhood",
                         "any-city",
                         "any-state"));
+    }
+
+    public static List<Gym> buildListOfGyms() {
+        var gym1 = Gym.createNewGym("First-gym-name",
+                "first-phone-number",
+                GymAddress.newAddress("First-gym-street-name",
+                        "any-number",
+                        "any-neighborhood",
+                        "any-city",
+                        "any-state"));
+
+        var gym2 = Gym.createNewGym("Second-gym-name",
+                "second-phone-number",
+                GymAddress.newAddress("Second-gym-street-name",
+                        "another-number",
+                        "another-neighborhood",
+                        "another-city",
+                        "another-state"));
+
+        return List.of(gym1, gym2);
     }
 }
